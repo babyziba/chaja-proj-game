9dbed3730fc9429ec01d533035b511af98ebb566
änita <62047664+babyziba@users.noreply.github.com>
2025-10-06 13:40:59 -0700

    Update click.cs
    added color changing

--- click.cs
+++ click.cs
@@ -1,25 +1,65 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
 
 
 public class EventClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+
+
     public player player;
-        public void OnPointerDown(PointerEventData eventData){
+    Renderer environmentObj;
+    public void OnPointerDown(PointerEventData eventData)
+    {
         Debug.Log("Pointer down: " + gameObject.name);
+
+        if (player.A_ActionSlots == 6)
+        {
+            Debug.Log("INV FULL BRO");
+            return;
+        }
+        
         player.A_ActionSlots += 1;
+        player.invState = 0;
+
+
+        if (player.A_ActionSlots == 3)
+        {
+            player.invState = 2;
+            environmentObj.material.color = Color.orange;
+
+        }
+        if (player.A_ActionSlots == 6)
+        {
+            player.invState = 3;
+            environmentObj.material.color = Color.red;
+
+        }
+
+
         Debug.Log("A_ActionSlots Value = " + player.A_ActionSlots);
     }
-    public void OnPointerUp(PointerEventData eventData){
+    public void OnPointerUp(PointerEventData eventData)
+    {
         Debug.Log("Pointer up: " + gameObject.name);
     }
-    public void OnPointerClick(PointerEventData eventData){
+    public void OnPointerClick(PointerEventData eventData)
+    {
         Debug.Log("Pointer click: " + gameObject.name);
     }
-    public void OnPointerEnter(PointerEventData eventData){
+    public void OnPointerEnter(PointerEventData eventData)
+    {
         Debug.Log("Pointer enter: " + gameObject.name);
     }
-    public void OnPointerExit(PointerEventData eventData){
+    public void OnPointerExit(PointerEventData eventData)
+    {
         Debug.Log("Pointer exit: " + gameObject.name);
     }
+    void Start()
+    {
+        environmentObj = GetComponent<MeshRenderer>();
+        environmentObj.material.color = Color.green;
+    }
+    
 }
